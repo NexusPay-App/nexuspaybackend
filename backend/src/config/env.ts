353e2036b6f76527bd26531d086c452199e7e7a8
RaphaelNdonga <nkibi53@gmail.com>
2024-10-13 21:20:56 +0300

    added arbitrum details to config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -9,19 +9,40 @@ let config: Record<string, any> = {
         THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY as string,
         AFRICAS_TALKING_API_KEY: process.env.AFRICAS_TALKING_API_KEY as string,
         MONGO_URL: process.env.MONGO_URL as string,
-        celo: defineChain(44787)
+        celo: {
+            chainId: 44787,
+            tokenAddress: "0x3572c9ce620f80032Ee3b101d75300186a0D7787"
+        },
+        arbitrum: {
+            chainId: 421614,
+            tokenAddress: "0x4e2Bd3a78bd9F064B7551F078f0Dde4Edab86238",
+        }
     },
     production: {
         THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY as string,
         AFRICAS_TALKING_API_KEY: process.env.AFRICAS_TALKING_API_KEY as string,
         MONGO_URL: process.env.MONGO_URL as string,
-        celo: defineChain(42220)
+        celo: {
+            chainId: 42220,
+            tokenAddress: "0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e"
+        },
+        arbitrum: {
+            chainId: 42161,
+            tokenAddress: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+        }
     },
     test: {
         THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY as string,
         AFRICAS_TALKING_API_KEY: process.env.AFRICAS_TALKING_API_KEY as string,
         MONGO_URL: process.env.MONGO_URL as string,
-        celo: defineChain(44787)
+        celo: {
+            chainId: 44787,
+            tokenAddress: "0x3572c9ce620f80032Ee3b101d75300186a0D7787"
+        },
+        arbitrum: {
+            chainId: 421614,
+            tokenAddress: "0x4e2Bd3a78bd9F064B7551F078f0Dde4Edab86238",
+        }
     }
 }
 
